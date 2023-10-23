300da14d94f2f1684fdaf53de41cc6a280ae11eb
felidae <115602897+felidae75@users.noreply.github.com>
2023-10-23 13:26:21 +0300

    upload test pages

--- newsport/pages/models.py
+++ newsport/pages/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 from django.db.models import Sum
 # Последнее - чтобы суммировало рейтинг
 
