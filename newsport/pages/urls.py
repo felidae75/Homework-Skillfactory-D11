5f6def747c1fa2bfaf68792178587dbe1868fb9f
felidae <115602897+felidae75@users.noreply.github.com>
2023-10-24 00:51:47 +0300

    + create paginator for pages

--- newsport/pages/urls.py
+++ newsport/pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import *
 
+app_name = 'pages'
 urlpatterns = [
     path('', PostView.as_view()),
-    path('<int:pk>', PostDetail.as_view()),
+    path('<int:pk>/', PostDetail.as_view()),
+    path('news/', PostsSort.as_view()),
 ]
--- newsport/pages/views.py
+++ newsport/pages/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
+from django.views import View
+
 from .models import *
 
 
@@ -7,10 +10,24 @@ class PostView(ListView):
     model = Post
     template_name = 'pages/news.html'
     context_object_name = 'news'
-    queryset = Post.objects.order_by('-date')
+    ordering = ['-date']
+    paginate_by = 1
 
 
 class PostDetail(DetailView):
     model = Post
     template_name = 'pages/post.html'
     context_object_name = 'post'
+
+
+class PostsSort(View):
+    def get(self, request):
+        posts = Post.objects.order_by('-date')
+        p_paginator = Paginator(posts, 1)
+        posts = p_paginator.get_page(request.GET.get('page', 1))
+
+        data = {
+            'posts': posts,
+        }
+
+        return render(request, 'pages/news.html', data)
