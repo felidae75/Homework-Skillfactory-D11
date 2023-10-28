c8c9a29b8b9e77812d16082d58353bd1fc3ce02d
felidae <115602897+felidae75@users.noreply.github.com>
2023-10-28 19:26:25 +0300

    Delete newsport/appointment directory

--- newsport/appointment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.urls import path
-from .views import *
-
-app_name = 'appointment'
-
-urlpatterns = [
-    path('', AppointmentView.as_view(), name="appointment"),
-]
